b179bddd3e209d10f4838ecb7fdd573fd17a30b7
vkuryachenko <vkuryachenko@gmail.com>
2016-03-19 10:12:31 +0600

    New model CUserChoice

--- polls/models.py
+++ polls/models.py
@@ -4,7 +4,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
-
+from cuser.models import CUser
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -17,6 +17,7 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -24,3 +25,8 @@ class Choice(models.Model):
     
     def __unicode__(self):
         return self.choice_text
+
+class CUserChoce(models.Model):
+    question= models.ForeignKey(Question, on_delete=models.CASCADE)
+    cuser = models.ForeignKey(CUser, on_delete=models.CASCADE)
+    date_vote = models.DateField()
