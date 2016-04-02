98b214a7eac833001a3e44be3ad3f3fe3e173ab9
vkuryachenko <vkuryachenko@gmail.com>
2016-04-02 18:26:41 +0600

    Normalizatoin image file name

--- polls/models.py
+++ polls/models.py
@@ -1,15 +1,28 @@
 from __future__ import unicode_literals
 
+import os
 import datetime
-
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from cuser.models import CUser
 
+def normalization_file_name(instance, filename):
+    new_name = 1
+    for root, dirs, files in os.walk(settings.MEDIA_ROOT):
+        for i in files:
+            f = os.path.splitext(i)[0]
+            if f.isdigit():
+                if int(f) >= new_name:
+                    new_name =  int(f)+1                     
+        break
+    return "{}{}".format(new_name, os.path.splitext(filename)[1])
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    question_img = models.ImageField(blank=True)
+    question_img = models.ImageField(blank=True, upload_to=normalization_file_name)
     
     def __unicode__(self):
         return self.question_text
